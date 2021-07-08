7f44545176fc80fe0380c54f7325ae709961b98b
Florian Le Guillou florian.leguillou@univ-grenoble-alpes.fr <leguilfl@f-dahu.u-ga.fr>
2021-07-08 16:29:53 +0200

    Minor modifications

--- mapping/src/tools_4Dvar.py
+++ mapping/src/tools_4Dvar.py
@@ -65,9 +65,10 @@ class Obsopt:
         
         delayed_results = []
         for t in self.date_obs:
-            res = delayed(self.process_obs)(t)
-            delayed_results.append(res)
-        compute(*delayed_results, scheduler="processes")
+            self.process_obs(t)
+            #res = delayed(self.process_obs)(t)
+            #delayed_results.append(res)
+        #compute(*delayed_results, scheduler="processes")
 
     
             
